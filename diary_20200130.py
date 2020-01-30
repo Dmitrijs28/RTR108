570bd9f6c65c96a83536f8a46098f24f07e2a213
user <blackbizon45@gmail.com>
2020-01-30 11:51:40 +0200

    20200130_11_50

---
+++ diary_20200130.py
@@ -0,0 +1,13 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: /home/user/RTR108/python_tests_auto_exe.py ============
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_tests_auto_exe.py'}
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/RTR108/python_tests_auto_exe.py'}
+-0.8390715290764524
+-0.8390715290764524
+-0.8390715290764524
+-0.8390715290764524
+-0.8390715290764524
+>>> 
---
+++ python_tests.py
@@ -0,0 +1,5 @@
+print vars()
+print(vars())
+print("%s"%(vars()))
+a=10
+vars("%s"%(vars()))
